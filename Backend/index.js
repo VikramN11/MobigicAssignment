df5db5b30c990766376eac514dda0c70d830b604
Vikram Navik <vikramnavik98@gmail.com>
2023-12-12 13:51:11 +0530

    multer 2

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 8000;
 const app = express();
 
 app.use(cors({
-    origin: ['https://6578108432cd9e2739d2b4b7--steady-pavlova-7b429b.netlify.app'],
+    origin: 'https://6578108432cd9e2739d2b4b7--steady-pavlova-7b429b.netlify.app',
   }));
 
 app.use(express.json());
